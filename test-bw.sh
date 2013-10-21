2782705239b201d60a4742cac51f21d9a6cc869f
Dexter Ang <i4dexter@users.noreply.github.com>
2013-10-21 12:42:46 +0800

    Modified test-bw.sh to use arrays and a for loop instead.

--- test-bw.sh
+++ test-bw.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 #
 # Script to test the speed of our ISP.
-# 
+#
 # Conversion to Mbps taken from:
 #  http://www.fir3net.com/General-UNIX/test-bandwith-on-linux.html
 #
 
-UBUNTUVER="13.04"
+declare -a labels=(
+	"BayanTel - Quezon City"
+	"Eastern Telecom - Makati City"
+	"Globe Telecom - Makati City"
+	"PLDT - Makati City"
+	"wi-tribe - Makati City"
+	"WiredTree - Chicago"
+	"SoftLayer - Washington DC"
+	)
+
+declare -a urls=(
+	"http://vega.skyinet.net/speedtest/random2500x2500.jpg"
+	"http://speedtest.eastern-tele.com/mini/speedtest/random2500x2500.jpg"
+	"http://speedtest.globe.com.ph/speedtest/random2500x2500.jpg"
+	"http://210.213.242.46/speedtest/speedtest/random2500x2500.jpg"
+	"http://www.wi-tribe.ph/speedtest/random2500x2500.jpg"
+	"http://speedtest.wiredtree.com/10mb.bin"
+	"http://speedtest.wdc01.softlayer.com/downloads/test10.zip"
+	)
+
+# ---------- DO NOT CHANGE ANYTHING BELOW ----------
+
 PARAMETER="--progress-bar"
 
 if [ "$1" == "silent" ]; then
@@ -15,37 +36,19 @@ else
 	echo 'To run silently, use parameter "silent".'
 fi
 
-echo "Speed downloading from WiredTree"
-echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null http://speedtest.wiredtree.com/10mb.bin` / 131072" | bc | xargs -I {} echo {}Mb\/s
-echo ""
-echo "--------------------------------------------------"
-echo ""
-echo "Speed downloading Ubuntu ISO from PREGINET"
+for (( i=0; i<${#labels[@]}; i++ )); do
+	echo "Speedtest from: ${labels[$i]}"
+	echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null ${urls[$i]}` / 131072" | bc | xargs -I {} echo {}Mb\/s
+	echo ""
+	echo "--------------------------------------------------"
+	echo ""
+done
+
+UBUNTUVER="13.04"
+
+# Special case: download Ubuntu ISO within a certain range only.
+echo "Speedtest from: Ubuntu ISO from PREGINET"
 echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null -r 0-10485760 http://mirror.pregi.net/pub/Linux/ubuntu-iso/$UBUNTUVER/ubuntu-$UBUNTUVER-desktop-amd64.iso` / 131072" | bc | xargs -I {} echo {}Mb\/s
 echo ""
-echo "--------------------------------------------------"
-echo ""
-echo "Speed downloading random2500x2500.jpg from Eastern Telecom"
-echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null http://speedtest.eastern-tele.com/mini/speedtest/random2500x2500.jpg` / 131072" | bc | xargs -I {} echo {}Mb\/s
-echo ""
-echo "--------------------------------------------------"
-echo ""
-echo "Speed downloading random2500x2500.jpg from PLDT"
-echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null http://210.213.242.46/speedtest/speedtest/random2500x2500.jpg` / 131072" | bc | xargs -I {} echo {}Mb\/s
-echo ""
-echo "--------------------------------------------------"
-echo ""
-echo "Speed downloading random2500x2500.jpg from Globe Telecom"
-echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null http://speedtest.globe.com.ph/speedtest/random2500x2500.jpg` / 131072" | bc | xargs -I {} echo {}Mb\/s
-echo ""
-echo "--------------------------------------------------"
-echo ""
-echo "Speed downloading random2500x2500.jpg from wi-tribe"
-echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null http://www.wi-tribe.ph/speedtest/random2500x2500.jpg` / 131072" | bc | xargs -I {} echo {}Mb\/s
-echo ""
-echo "--------------------------------------------------"
-echo ""
-echo "Speed downloading test10.zip from SoftLayer"
-echo "scale=2; `curl $PARAMETER -w "%{speed_download}" -o /dev/null http://speedtest.wdc01.softlayer.com/downloads/test10.zip` / 131072" | bc | xargs -I {} echo {}Mb\/s
-echo ""
+
 echo "fin."
